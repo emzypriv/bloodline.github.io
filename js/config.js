2d3624334720dab005045ddcffdc41ad0c7ffaae
Emzy1001 <102365384+Emzy1001@users.noreply.github.com>
2026-03-29 13:15:55 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,14 +5,14 @@
 // Create a project → Add Web App → Copy config below
 // ============================================================
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyC6El4QT2--ecQNWV18e-4L4s26iol_eUk",
-    authDomain: "bloodline-efc76.firebaseapp.com",
-    projectId: "bloodline-efc76",
-    storageBucket: "bloodline-efc76.firebasestorage.app",
-    messagingSenderId: "97046048461",
-    appId: "1:97046048461:web:e1bdc760f130fb72830384"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyC6El4QT2--ecQNWV18e-4L4s26iol_eUk",
+  authDomain: "bloodline-efc76.firebaseapp.com",
+  projectId: "bloodline-efc76",
+  storageBucket: "bloodline-efc76.firebasestorage.app",
+  messagingSenderId: "97046048461",
+  appId: "1:97046048461:web:e1bdc760f130fb72830384"
+};
 
 // ============================================================
 // SETUP INSTRUCTIONS:
